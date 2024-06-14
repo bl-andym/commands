1b7a4d5fe0ff9a4dd505640b8ce85958a501e9a4
AndyM <marsden.a@gmail.com>
2024-06-14 11:12:17 +0100

    mongoClinet

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 if (!uri) {
-    throw new Error('Please define the MONGODB_URI environment variable');
+    throw new Error('environment variable undefined!');
 }
 const client = new MongoClient(uri, {
     serverApi: {
